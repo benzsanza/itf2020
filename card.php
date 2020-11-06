c34365fc21755de39da599880c590a04d6eace98
benzsanza <73688808+benzsanza@users.noreply.github.com>
2020-11-06 17:32:40 +0700

    Add files via upload

---
+++ card.php
@@ -0,0 +1,47 @@
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<title>plswork</title>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col-4">
+            <div class="card">
+                <img class="card-img-top" src="img/5.jpg" >
+                <div class="card-body">
+                    <h4 class="card-title">Fate Grand Order</h4>
+                    <p class="card-text">ภาคที่นิยมมากที่สุดของ Fate และมีเกมออกมา !<p>
+                        <div class="card-footer">
+                            บ่นโดย นาย ธนาดุล เสริมเผือก
+                        </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-4">
+            <div class="card">
+                <img class="card-img-top" src="img/4.jpg" >
+                <div class="card-body">
+                    <h4 class="card-title">Fate stay night Unlimater blane world</h4>
+                    <p class="card-text">เป็น Fate ที่ผมชอบมากสุด</p>
+                    <div class="card-footer">
+                        บ่นโดย นาย ธนาดุล เสริมเผือก
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-4">
+            <div class="card">
+                <img class="card-img-top" src="img/2.jpg" >
+                <div class="card-body">
+                    <h4 class="card-title">Fate stay night night heaven's feel</h4>
+                    <p class="card-text">ภาคเดอะมูฟวี่ Fate stay night ที่พึ่งฉายในไทย</p>
+                    <div class="card-footer">
+                        บ่นโดย นาย ธนาดุล เสริมเผือก
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>    
+</body>
+</html>
